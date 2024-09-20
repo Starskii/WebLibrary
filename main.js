ede3cba0a00b1ccbb4654dcc17cf0efa1b46651a
Starskii <jakeylakie@yahoo.com>
2024-09-19 22:04:47 -0700

    added addBookToLibrary function

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author, pages, hasRead){
     this.title = title;
     this.author = author;
@@ -8,3 +10,11 @@ function Book(title, author, pages, hasRead){
         return `The ${this.title} by ${this.author}, ${this.pages} pages, ${this.hasRead ? "has been read." : "has not been read yet."}`
     }
 }
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+const myBook = new Book("a", "a", 100, false);
+addBookToLibrary(myBook);
+console.log(myLibrary);
