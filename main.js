cb3c8baac50da5c6f07a73b5f4fe8d3f8f6400df
Starskii <jakeylakie@yahoo.com>
2024-09-22 20:25:08 -0700

    added add-book-modal

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const myLibrary = [];
 
+const modal = document.querySelector(".book-input-modal");
+
 function Book(title, author, pages, hasRead){
     this.title = title;
     this.author = author;
@@ -14,6 +16,21 @@ function Book(title, author, pages, hasRead){
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
+
+function onAddBookButtonPressed(e){
+     modal.classList.remove("hidden");
+}
+
+function onCancelButtonPressed(e){
+    modal.classList.add("hidden"); 
+}
+
+const addBookBtn = document.getElementById("add-book");
+const exitModalBtn = document.getElementById("exit-button");
+const cancelModalBtn = document.getElementById("cancel-button");
+addBookBtn.addEventListener("click", onAddBookButtonPressed);
+exitModalBtn.addEventListener("click", onCancelButtonPressed);
+cancelModalBtn.addEventListener("click", onCancelButtonPressed);
 
 const myBook = new Book("a", "a", 100, false);
 addBookToLibrary(myBook);
